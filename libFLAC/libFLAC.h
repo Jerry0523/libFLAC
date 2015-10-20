441796d9b17c144a0d49f14d606dafceb9e3ad34
Jerry <jaymee0523@gmail.com>
2015-10-20 09:43:18 +0800

    Initial Commit

---
+++ libFLAC/libFLAC.h
@@ -0,0 +1,19 @@
+//
+//  libFLAC.h
+//  libFLAC
+//
+//  Created by Jerry Wong's Mac Mini on 15/10/20.
+//  Copyright © 2015年 Jerry Wong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for libFLAC.
+FOUNDATION_EXPORT double libFLACVersionNumber;
+
+//! Project version string for libFLAC.
+FOUNDATION_EXPORT const unsigned char libFLACVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libFLAC/PublicHeader.h>
+
+
